bfe51677829ce6bdb99ecd0970c69a48934b2d42
Dwaipayan Sen <32829833+DiySane@users.noreply.github.com>
2026-05-02 18:52:26 +0000

    Update adapter to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import type { Config } from "@sveltejs/adapter-vercel";
 
-export default {
-  kit: {
-    // adapter: adapter(),
-  },
+// export default {
+//   kit: {
+//     // adapter: adapter(),
+//   },
+// };
+
+export const config: Config = {
+  split: true,
 };
