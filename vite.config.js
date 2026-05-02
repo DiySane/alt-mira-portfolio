2e1e94956d538acac36aa7bd68a24fbf4eae8b62
Dwaipayan Sen <32829833+DiySane@users.noreply.github.com>
2026-05-02 19:05:43 +0000

    UUpdate vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
+  build: {
+    // Change the output directory from 'dist' to 'build'
+    outDir: "public",
+  },
   plugins: [sveltekit()],
-};
+});
