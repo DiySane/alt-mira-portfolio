191ef01e67c11cbf0434802ccac9565185bda20e
Dwaipayan Sen <32829833+DiySane@users.noreply.github.com>
2026-05-03 07:21:44 +0000

    Update config to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
-// import adapter from "@sveltejs/adapter-auto";
-import Config from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-auto";
+// import Config from "@sveltejs/adapter-vercel";
 
-// export default {
-//   kit: {
-//     adapter: adapter(),
-//   },
-// };
-
-const config = {
-  split: true,
+export default {
+  kit: {
+    adapter: adapter(),
+  },
 };
 
-export default config;
+// const config = {
+//   split: true,
+//   outDir: "public",
+// };
+
+// export default config;
